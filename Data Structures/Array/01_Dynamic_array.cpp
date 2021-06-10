01c8a7c8f5ab31a15dd66c9846792ceafa646f8a
prathviraj b <pishere.learn@gmail.com>
2021-06-10 10:11:08 +0530

    Create 01_Dynamic_array.cpp

---
+++ Data Structures/Array/01_Dynamic_array.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  /*get place in stack*/
+  int n;
+  cin>>n;
+  int dynamic_arr_in_stk[n] = {1};
+  
+  for(int i=0;i<n;i++){
+    cout<<dynamic_arr_in_stk[i]<<endl;
+  }
+  
+  /*get place in heap*/
+  int *dynamic_arr_in_heap = new int[n] ;
+  
+  dynamic_arr_in_heap[0] = 2;
+  
+  for(int i=0;i<n;i++){
+    cout<<dynamic_arr_in_heap[i]<<endl;
+  }
+  
+  return 0;
+}
