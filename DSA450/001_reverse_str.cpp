99d7131fcc1391ce18526e3cbfb24336fd999d0a
pishere <prathvirajbn.cs18@sahyadri.edu.in>
2021-06-29 09:56:10 +0530

    dsa

---
+++ DSA450/001_reverse_str.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+string sol_w_extra_space(string str) {
+    string s;
+    int n = str.length();
+    for (int i = n; i >= 0; i--) {
+        s.push_back(str[i]);
+    }
+    return s;
+}
+
+void optimal_sol(string *str, int low, int high) {
+
+    if (low >= high) {
+        return;
+    }
+    char temp = (*str)[low];
+    (*str)[low] = (*str)[high];
+    (*str)[high] = temp;
+    optimal_sol(str, low + 1, high - 1);
+}
+
+int main() {
+    string str = "Crack it.";
+    cout<<sol_w_extra_space(str)<<endl; //space O(n) time O(n)
+    optimal_sol(&str, 0, str.length() - 1); //space O(1) time n/2 => O(n)
+    cout << str;
+    return 0;
+}
--- STLs/mapss.cpp
+++ STLs/mapss.cpp
@@ -1,15 +1,27 @@
 #include<iostream>
 #include<map>
-#include<algorithm>
+#include<unordered_map>
+
 using namespace std;
 
-int main(){
+void display(map<char, int> m1){
+    
+    for(auto x:m1){
+        cout<<x.first<<":"<<x.second<<endl;
+    }
+}
+void display(unordered_map<char, int> m1){
+    
+    for(auto x:m1){
+        cout<<x.first<<":"<<x.second<<endl;
+    }
+}
+
+void ordered_map(){
     
     map<int, int> m;
-    
     m[2] = 20;//logn
     cout<<m[2]<<endl;//logn
-
     map<char, int> m1;
 
     string a = "abbaa";
@@ -18,9 +30,29 @@ int main(){
         m1[x]++;
     }
 
-    for(auto x:m1){
-        cout<<x.first<<":"<<x.second<<endl;
+    display(m1);
+
+}
+
+void un_ordered_map(){
+    
+
+    unordered_map<char, int> m1;
+
+    string a = "abbaa";
+    
+    for(auto x:a ){
+        m1[x]++;
     }
+
+    display(m1);
+}
+
+
+int main(){
+    
+    ordered_map(); //insert clear takes O(logn)  implimentation as BST 
+    un_ordered_map(); //insert takes O(n) clear takes O(1) because implimentation as hashing open hashing
 
     return 0;
 }
