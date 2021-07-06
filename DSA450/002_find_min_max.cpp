8b8d3a945a00a63a120f9d6d67683e78772d7b7c
pishere <prathvirajbn.cs18@sahyadri.edu.in>
2021-07-06 15:15:58 +0530

    s

--- DSA450/002_find_min_max.cpp
+++ DSA450/002_find_min_max.cpp
@@ -7,32 +7,62 @@ void bruteForce_sol(vector<int> list){
     int min = list[0];
     int max = list[0];
 
-    if(list.capacity() == 1){
+    if(list.size() == 1){
         cout<<"Both min and max : "<<list[0]<<endl;
+        cout<<"Comparisions: 0"<<endl;
+
+    }
+    if(list.size() == 2){
+        int min = 0;
+        int max = 0;
+
+        if(list[0]<list[1]){
+            min = list[0];
+            max = list[1];
+        } else{
+            min = list[1];
+            max = list[0];
+        }
+        cout<<"min and max : "<<min<<" "<<max<<endl;
+        cout<<"Comparisions: 1"<<endl;
     }
     //for(auto X:list)
-    for(auto it = list.begin();it!=list.end();it++){
-        if(*it>max){
+    int c =0;
+    for(auto it = list.begin()+1;it!=list.end();it++){
+        c++;
+        if(*it>max){ 
+            
             max = *it;
         }
+        c++;
         if(*it<min){
+            
             min = *it;
         }
     }
     cout<<"Max : "<<max<<endl;
     cout<<"Min : "<<min<<endl;
+    cout<<"Comparisions: "<<c<<endl;
+
 
 }
 
-void tournament_method(vector<int> list){
-    
+class maxMin{
+    int max,min;
+void tournament_method(vector<int> list,int low,int high){
+    if(low == high){
+        
+    }
 }
+};
+
 
 int main(){
 
     vector<int> list= {1,2,3,8,6,5,4,12};
-    bruteForce_sol(list); //O(n)
-    tournament_method(list);
+    bruteForce_sol(list); //O(n)  no of comparisions 2n-2
+    
+    tournament_method(list,0,(list.size()-1)/2,list.size()-1);
 
     return 0;
 }
